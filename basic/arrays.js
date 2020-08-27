ff823b9a1b0eebfe2c7367cad5de6ba540a4c27f
Ignacio Sbaraglia <shogakukan@gmail.com>
2020-08-26 22:30:13 -0300

    segundo mayor

--- basic/arrays.js
+++ basic/arrays.js
@@ -75,17 +75,17 @@ function segundoMayor(array){
     array.sort((a, b) => b - a); //orden descendente
     let i = 0;
     let distintos = false;
-    while (i < array.length-1 && !distintos){
-        if (array[i] === array [i+1]){
-            i++; 
-        } else {
-            distintos = true;
-        }
-    }
-    return array[i+1]; //si todos los numeros son iguales, me devuelve undefined ¿está ok eso?
+    return array.find(num => num < array[0]);
+    // while (i < array.length-1 && !distintos){
+    //     if (array[i] === array [i+1]){
+    //         i++; 
+    //     } else {
+    //         distintos = true;
+    //     }
+    // }
+    // return array[i+1]; //si todos los numeros son iguales, me devuelve undefined ¿está ok eso?
 }
-
+console.log(segundoMayor([1,1]));
 console.log(segundoMayor([2,4,5,6]) === 5);
 console.log(segundoMayor([4,6,7,8,7]) === 7);
 console.log(segundoMayor([4,6,7,7]) === 6);
-
